acc9fdf45ad32b64596af38dd49e6c65888479e1
Pablo Gargallo <pablogargallosanz@gmail.com>
2025-03-17 13:03:55 +0100

    Feat: Menu interactivo

--- src/App.java
+++ src/App.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
 public class App {
+  
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        Scanner sc = new Scanner(System.in);
+        int opciones;
+		
+        do {
+		
+			System.out.println("Vienvenido a la gestión del restaurante");
+			System.out.println("Opción 1: Registrar mesa");
+			System.out.println("Opción 2: Registrar plato");
+			System.out.println("Opción 3: Regitrar pedido");
+			System.out.println("Opción 4: Modificar pedido");
+			System.out.println("Opción 5: Borrar pedido");
+			System.out.println("Opción 6 Modificar o borrar plato");
+			System.out.println("Opción 7: Guradar datos");
+			System.out.println("Opción 8: Cargar datos");
+			System.out.println("Opción 9: Salir");
+			
+			opciones = sc.nextInt();
+			
+			switch(opciones) {
+			case 1 -> {}
+			case 2 -> {}
+			case 3 -> {}
+			case 4 -> {}
+			case 5 -> {}
+			case 6 -> {}
+			case 7 -> {}
+			case 8 -> {}
+			}
+		
+		// Se ejecuta siempre que no se introduzca el 9
+		}while (opciones != 9);
+		
+		
+	}
 }
